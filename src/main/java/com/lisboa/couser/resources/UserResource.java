791eb321ce62147fe8dcdbcd0cdb5b57709f8a4a
gulisboa <gu.plisboa@gmail.com>
2020-08-10 21:07:26 -0300

    user entities and resources

---
+++ src/main/java/com/lisboa/couser/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.lisboa.couser.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lisboa.couser.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@email.com", "3466-1520", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+
+}
